3fb096a0c2624ad67de6e5427abb7fe2d9000936
Vasily Krylov <tor@outsorcerers.com>
2017-02-19 01:17:47 +0300

    Добавил diff'ы сборок и описание

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ npm run watch
 
 
 
-## Questions
+## Problem/Questions
 
 На сайте [офицальной документации](about.bundle.js) сказано
 
@@ -37,22 +37,35 @@ npm run watch
   application is spread out over multiple files.
 ```
 
-* Когда и при каких услоавиях `babel` генерирует эти вспомогательные функции
-  * https://babeljs.io/docs/plugins/transform-es2015-computed-properties/#example
-    `{[val]: val}` -> `function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }({}, text + '_' + text, text)`
+* **Когда и при каких услоавиях `babel` генерирует эти вспомогательные функции**
+  * Например, в случае использования [`Computed property names`](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Object_initializer)
+    [Пример из документации babel](https://babeljs.io/docs/plugins/transform-es2015-computed-properties/#example)
 
-* Когда нужно подключать `babel-polyfill` (как я пониманию что мне нужен полифил?) смотрю [ECMAScript 6 compatibility table](https://kangax.github.io/compat-table/es6/) для нужной мне фичи и если она не поддерживается где мне нужно, то уже тогда думаю о подключение полифила, так получается?
-  * куда их подключать (создать в `webpack.config` `entry` `common`?) и там перечислить то что нужно `common: ['a', 'b', 'c']`
+Для следующего кода, будет создана функция хелпер `_defineProperty`
+
+```js
+{[val]: val}
+```
+
+И в результате получится
+
+```js
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+_defineProperty({}, text + '_' + text, text);
+```
+
+* **Когда нужно подключать `babel-polyfill` (как я пойму, что мне нужен полифил?)**
+  * Посмотреть [ECMAScript 6 compatibility table](https://kangax.github.io/compat-table/es6/) для нужного мне функционала, и если таковой не поддерживается в нужном мне окружении, то уже тогда думаю о подключение полифила, так получается?
+  * куда их подключать (создать в `webpack.config` `entry` `common`?) и там перечислить то, что нужно `common: ['a', 'b', 'c']`
   * как сделать так, чтобы они не аффектили, те случаи, когда в окружение уже есть нужный мне функционал
   * можно ли как-то научить `babel` автоматически распозновать какие полифилы мне нужны
 
 
-
+## Solution/Investigation 🔦
 
 Получаем дублирование кода
 
-
-(i used `diff` that highlight lines, on the case code didn't have `-` symbols)
+(I used `diff` that highlight lines, on the case code didn't have `-` symbols)
 <details>
 <summary>```/******/ (function(modules) { // webpackBootstrap```</summary>
 ```js
@@ -169,3 +182,119 @@ npm run watch
   /******/ ]);
   //# sourceMappingURL=home.bundle.js.map
 ```
+
+
+Теперь сделаем сборку с [`babel-plugin-transform-runtime`](https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-runtime).
+Для этого, как сказанно в [документации](https://babeljs.io/docs/plugins/transform-runtime/#installation), нужно добавить `babel-plugin-transform-runtime` в зависимости:
+
+```bash
+npm install --save-dev babel-plugin-transform-runtime
+```
+
+И указать в `.babelrc` `"plugins": ["transform-runtime"]`. После этих манипуляций мой `.babelrc` выглядит так:
+
+```json
+{
+  "presets": [
+    [ "es2015", {"modules": false}]
+  ],
+  "plugins": [
+    "transform-runtime"
+  ]
+}
+```
+
+Небольшое отступление... Как я понял, `babel-runtime` необязательно ставить в `dependency` (_"as a production dependency"_ - как сказано в документации), просто это семантически верно, т.к. именно код из `babel-runtime` попадает в сборку, а вообще `babel-runtime` устанавливается как зависимость для `babel-core` и для `babel-plugin-transform-runtime` соответственно. (да и вообще для всех `babel-*` пакетов (попробуйте `grep -R --include=package.json babel-runtime node_modules`)). А также `babel-runtime` имеет в `devDependency` `babel-plugin-transform-runtime`.
+
+
+В результате сборки у меня получились такие diff'ы: [about-with-without.diff](builds/about-with-without.diff) и, **наиболее интересный (потому что имеено тут было дублирование кода) [home-with-without.diff](builds/home-with-without.diff)** (можно заметить, что теперь мы избежали дублирования кода, но зато, появился какой-то лишний)
+
+Результаты сборки:
+* **without transform runtime**
+  ![](without-transform-runtime.png)
+* **with transform runtime**
+  ![](with-transform-runtime.png)
+
+
+
+Теперь немного поэксперементируем со сборкой...
+
+За нужное нам преобразование отвечает [`babel-plugin-transform-es2015-computed-properties`](https://babeljs.io/docs/plugins/transform-es2015-computed-properties), подключим только его и убедимся, что результат соответсвует тому, как если бы подключили `babel-preset-es2015` содержащий этот плагин. Тогда `.babelrc` будет выглядеть следующим образом
+
+```json
+{
+  "plugins": [
+    ["transform-es2015-computed-properties"]
+  ]
+}
+```
+
+Как и ожидалось в коде появилось дублирование `_defineProperty`
+
+```js
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+```
+
+Посмотрим как еще можно от этого избавиться...
+
+У этого плагина [`babel-plugin-transform-es2015-computed-properties`](https://babeljs.io/docs/plugins/transform-es2015-computed-properties) есть параметр `loose`, который указывает использовать упрощенный синтаксис, менее соответвующей спецификации ES6 ([можно почитать тут](http://www.2ality.com/2015/12/babel6-loose-mode.html)). Получаются следующие преобразования:
+
+Код модуля `welcome` до преобразований
+
+```js
+export function welcome(text) {
+    return {[text + '_' + text]: text};
+}
+```
+
+Код модуля `welcome` **без использования** флага `loose`
+
+```js
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function welcome(text) {
+    return _defineProperty({}, text + '_' + text, text);
+}
+```
+
+Код модуля `welcome` **c использованием** флага `loose`
+
+```js
+function welcome(text) {
+    var _ref;
+
+    return _ref = {}, _ref[text + '_' + text] = text, _ref;
+}
+```
+
+Итого, мы избавились от дублирования, код стал проще, но не значит лучше, теперь он не совсем соответсвует спецификации, и это не в нашу пользу, в теории могут какие-то сайд эффекты.
+
+И все же, вот список плагинов поддерживающих `loose`
+
+* [transform-es2015-classes](http://babeljs.io/docs/plugins/transform-es2015-classes)
+* [transform-es2015-computed-properties](http://babeljs.io/docs/plugins/transform-es2015-computed-properties)
+* [transform-es2015-for-of](http://babeljs.io/docs/plugins/transform-es2015-for-of)
+* [transform-es2015-spread](http://babeljs.io/docs/plugins/transform-es2015-spread)
+* [transform-es2015-destructuring](http://babeljs.io/docs/plugins/transform-es2015-destructuring)
+* [transform-es2015-modules-commonjs](http://babeljs.io/docs/plugins/transform-es2015-modules-commonjs)
+* [transform-es2015-modules-systemjs](http://babeljs.io/docs/plugins/transform-es2015-modules-systemjs)
+* [transform-es2015-modules-amd](http://babeljs.io/docs/plugins/transform-es2015-modules-amd)
+* [transform-es2015-modules-umd](http://babeljs.io/docs/plugins/transform-es2015-modules-umd)
+
+
+Кстати, при следующей конфигурации `.babelrc` в сборку так же не попадут никакие helper'ы babel, потому необходимые пребразования выполнены с флагом `loose`, а соответвенно helper'ов для них не нужно. Полезная возможность точечно указать к каким плагинам, какие параметры применять.
+
+```json
+{
+  "presets": [
+    [ "es2015", {"modules": false}]
+  ],
+  "plugins": [
+    ["transform-runtime"],
+    ["transform-es2015-computed-properties", {
+      "loose": true
+    }]
+  ]
+}
+```
+
---
+++ builds/with-babel-transfrom-runtime/home.bundle.js
@@ -0,0 +1,502 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 21);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/* unknown exports provided */
+/* all exports used */
+/*!***************************************************!*\
+  !*** ./~/core-js/library/modules/_descriptors.js ***!
+  \***************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+// Thank's IE8 for his funny defineProperty
+module.exports = !__webpack_require__(/*! ./_fails */ 3)(function(){
+  return Object.defineProperty({}, 'a', {get: function(){ return 7; }}).a != 7;
+});
+
+/***/ }),
+/* 1 */
+/* unknown exports provided */
+/* all exports used */
+/*!*************************************************!*\
+  !*** ./~/core-js/library/modules/_is-object.js ***!
+  \*************************************************/
+/***/ (function(module, exports) {
+
+module.exports = function(it){
+  return typeof it === 'object' ? it !== null : typeof it === 'function';
+};
+
+/***/ }),
+/* 2 */
+/* unknown exports provided */
+/* all exports used */
+/*!********************************************!*\
+  !*** ./~/core-js/library/modules/_core.js ***!
+  \********************************************/
+/***/ (function(module, exports) {
+
+var core = module.exports = {version: '2.4.0'};
+if(typeof __e == 'number')__e = core; // eslint-disable-line no-undef
+
+/***/ }),
+/* 3 */
+/* unknown exports provided */
+/* all exports used */
+/*!*********************************************!*\
+  !*** ./~/core-js/library/modules/_fails.js ***!
+  \*********************************************/
+/***/ (function(module, exports) {
+
+module.exports = function(exec){
+  try {
+    return !!exec();
+  } catch(e){
+    return true;
+  }
+};
+
+/***/ }),
+/* 4 */
+/* unknown exports provided */
+/* all exports used */
+/*!**********************************************!*\
+  !*** ./~/core-js/library/modules/_global.js ***!
+  \**********************************************/
+/***/ (function(module, exports) {
+
+// https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
+var global = module.exports = typeof window != 'undefined' && window.Math == Math
+  ? window : typeof self != 'undefined' && self.Math == Math ? self : Function('return this')();
+if(typeof __g == 'number')__g = global; // eslint-disable-line no-undef
+
+/***/ }),
+/* 5 */
+/* unknown exports provided */
+/* all exports used */
+/*!*************************************************!*\
+  !*** ./~/core-js/library/modules/_object-dp.js ***!
+  \*************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var anObject       = __webpack_require__(/*! ./_an-object */ 11)
+  , IE8_DOM_DEFINE = __webpack_require__(/*! ./_ie8-dom-define */ 16)
+  , toPrimitive    = __webpack_require__(/*! ./_to-primitive */ 18)
+  , dP             = Object.defineProperty;
+
+exports.f = __webpack_require__(/*! ./_descriptors */ 0) ? Object.defineProperty : function defineProperty(O, P, Attributes){
+  anObject(O);
+  P = toPrimitive(P, true);
+  anObject(Attributes);
+  if(IE8_DOM_DEFINE)try {
+    return dP(O, P, Attributes);
+  } catch(e){ /* empty */ }
+  if('get' in Attributes || 'set' in Attributes)throw TypeError('Accessors not supported!');
+  if('value' in Attributes)O[P] = Attributes.value;
+  return O;
+};
+
+/***/ }),
+/* 6 */
+/* unknown exports provided */
+/* exports used: default */
+/*!***************************************************!*\
+  !*** ./~/babel-runtime/helpers/defineProperty.js ***!
+  \***************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+exports.__esModule = true;
+
+var _defineProperty = __webpack_require__(/*! ../core-js/object/define-property */ 8);
+
+var _defineProperty2 = _interopRequireDefault(_defineProperty);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (obj, key, value) {
+  if (key in obj) {
+    (0, _defineProperty2.default)(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+};
+
+/***/ }),
+/* 7 */
+/* exports provided: welcome */
+/* exports used: welcome */
+/*!************************!*\
+  !*** ./app/welcome.js ***!
+  \************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty__ = __webpack_require__(/*! babel-runtime/helpers/defineProperty */ 6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty__);
+/* harmony export (immutable) */ __webpack_exports__["a"] = welcome;
+
+
+
+function welcome(text) {
+    return __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty___default()({}, text + '_' + text, text);
+}
+
+/***/ }),
+/* 8 */
+/* unknown exports provided */
+/* all exports used */
+/*!***********************************************************!*\
+  !*** ./~/babel-runtime/core-js/object/define-property.js ***!
+  \***********************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = { "default": __webpack_require__(/*! core-js/library/fn/object/define-property */ 9), __esModule: true };
+
+/***/ }),
+/* 9 */
+/* unknown exports provided */
+/* all exports used */
+/*!********************************************************!*\
+  !*** ./~/core-js/library/fn/object/define-property.js ***!
+  \********************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../../modules/es6.object.define-property */ 19);
+var $Object = __webpack_require__(/*! ../../modules/_core */ 2).Object;
+module.exports = function defineProperty(it, key, desc){
+  return $Object.defineProperty(it, key, desc);
+};
+
+/***/ }),
+/* 10 */
+/* unknown exports provided */
+/* all exports used */
+/*!**************************************************!*\
+  !*** ./~/core-js/library/modules/_a-function.js ***!
+  \**************************************************/
+/***/ (function(module, exports) {
+
+module.exports = function(it){
+  if(typeof it != 'function')throw TypeError(it + ' is not a function!');
+  return it;
+};
+
+/***/ }),
+/* 11 */
+/* unknown exports provided */
+/* all exports used */
+/*!*************************************************!*\
+  !*** ./~/core-js/library/modules/_an-object.js ***!
+  \*************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var isObject = __webpack_require__(/*! ./_is-object */ 1);
+module.exports = function(it){
+  if(!isObject(it))throw TypeError(it + ' is not an object!');
+  return it;
+};
+
+/***/ }),
+/* 12 */
+/* unknown exports provided */
+/* all exports used */
+/*!*******************************************!*\
+  !*** ./~/core-js/library/modules/_ctx.js ***!
+  \*******************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+// optional / simple context binding
+var aFunction = __webpack_require__(/*! ./_a-function */ 10);
+module.exports = function(fn, that, length){
+  aFunction(fn);
+  if(that === undefined)return fn;
+  switch(length){
+    case 1: return function(a){
+      return fn.call(that, a);
+    };
+    case 2: return function(a, b){
+      return fn.call(that, a, b);
+    };
+    case 3: return function(a, b, c){
+      return fn.call(that, a, b, c);
+    };
+  }
+  return function(/* ...args */){
+    return fn.apply(that, arguments);
+  };
+};
+
+/***/ }),
+/* 13 */
+/* unknown exports provided */
+/* all exports used */
+/*!**************************************************!*\
+  !*** ./~/core-js/library/modules/_dom-create.js ***!
+  \**************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var isObject = __webpack_require__(/*! ./_is-object */ 1)
+  , document = __webpack_require__(/*! ./_global */ 4).document
+  // in old IE typeof document.createElement is 'object'
+  , is = isObject(document) && isObject(document.createElement);
+module.exports = function(it){
+  return is ? document.createElement(it) : {};
+};
+
+/***/ }),
+/* 14 */
+/* unknown exports provided */
+/* all exports used */
+/*!**********************************************!*\
+  !*** ./~/core-js/library/modules/_export.js ***!
+  \**********************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var global    = __webpack_require__(/*! ./_global */ 4)
+  , core      = __webpack_require__(/*! ./_core */ 2)
+  , ctx       = __webpack_require__(/*! ./_ctx */ 12)
+  , hide      = __webpack_require__(/*! ./_hide */ 15)
+  , PROTOTYPE = 'prototype';
+
+var $export = function(type, name, source){
+  var IS_FORCED = type & $export.F
+    , IS_GLOBAL = type & $export.G
+    , IS_STATIC = type & $export.S
+    , IS_PROTO  = type & $export.P
+    , IS_BIND   = type & $export.B
+    , IS_WRAP   = type & $export.W
+    , exports   = IS_GLOBAL ? core : core[name] || (core[name] = {})
+    , expProto  = exports[PROTOTYPE]
+    , target    = IS_GLOBAL ? global : IS_STATIC ? global[name] : (global[name] || {})[PROTOTYPE]
+    , key, own, out;
+  if(IS_GLOBAL)source = name;
+  for(key in source){
+    // contains in native
+    own = !IS_FORCED && target && target[key] !== undefined;
+    if(own && key in exports)continue;
+    // export native or passed
+    out = own ? target[key] : source[key];
+    // prevent global pollution for namespaces
+    exports[key] = IS_GLOBAL && typeof target[key] != 'function' ? source[key]
+    // bind timers to global for call from export context
+    : IS_BIND && own ? ctx(out, global)
+    // wrap global constructors for prevent change them in library
+    : IS_WRAP && target[key] == out ? (function(C){
+      var F = function(a, b, c){
+        if(this instanceof C){
+          switch(arguments.length){
+            case 0: return new C;
+            case 1: return new C(a);
+            case 2: return new C(a, b);
+          } return new C(a, b, c);
+        } return C.apply(this, arguments);
+      };
+      F[PROTOTYPE] = C[PROTOTYPE];
+      return F;
+    // make static versions for prototype methods
+    })(out) : IS_PROTO && typeof out == 'function' ? ctx(Function.call, out) : out;
+    // export proto methods to core.%CONSTRUCTOR%.methods.%NAME%
+    if(IS_PROTO){
+      (exports.virtual || (exports.virtual = {}))[key] = out;
+      // export proto methods to core.%CONSTRUCTOR%.prototype.%NAME%
+      if(type & $export.R && expProto && !expProto[key])hide(expProto, key, out);
+    }
+  }
+};
+// type bitmap
+$export.F = 1;   // forced
+$export.G = 2;   // global
+$export.S = 4;   // static
+$export.P = 8;   // proto
+$export.B = 16;  // bind
+$export.W = 32;  // wrap
+$export.U = 64;  // safe
+$export.R = 128; // real proto method for `library` 
+module.exports = $export;
+
+/***/ }),
+/* 15 */
+/* unknown exports provided */
+/* all exports used */
+/*!********************************************!*\
+  !*** ./~/core-js/library/modules/_hide.js ***!
+  \********************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var dP         = __webpack_require__(/*! ./_object-dp */ 5)
+  , createDesc = __webpack_require__(/*! ./_property-desc */ 17);
+module.exports = __webpack_require__(/*! ./_descriptors */ 0) ? function(object, key, value){
+  return dP.f(object, key, createDesc(1, value));
+} : function(object, key, value){
+  object[key] = value;
+  return object;
+};
+
+/***/ }),
+/* 16 */
+/* unknown exports provided */
+/* all exports used */
+/*!******************************************************!*\
+  !*** ./~/core-js/library/modules/_ie8-dom-define.js ***!
+  \******************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = !__webpack_require__(/*! ./_descriptors */ 0) && !__webpack_require__(/*! ./_fails */ 3)(function(){
+  return Object.defineProperty(__webpack_require__(/*! ./_dom-create */ 13)('div'), 'a', {get: function(){ return 7; }}).a != 7;
+});
+
+/***/ }),
+/* 17 */
+/* unknown exports provided */
+/* all exports used */
+/*!*****************************************************!*\
+  !*** ./~/core-js/library/modules/_property-desc.js ***!
+  \*****************************************************/
+/***/ (function(module, exports) {
+
+module.exports = function(bitmap, value){
+  return {
+    enumerable  : !(bitmap & 1),
+    configurable: !(bitmap & 2),
+    writable    : !(bitmap & 4),
+    value       : value
+  };
+};
+
+/***/ }),
+/* 18 */
+/* unknown exports provided */
+/* all exports used */
+/*!****************************************************!*\
+  !*** ./~/core-js/library/modules/_to-primitive.js ***!
+  \****************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+// 7.1.1 ToPrimitive(input [, PreferredType])
+var isObject = __webpack_require__(/*! ./_is-object */ 1);
+// instead of the ES6 spec version, we didn't implement @@toPrimitive case
+// and the second argument - flag - preferred type is a string
+module.exports = function(it, S){
+  if(!isObject(it))return it;
+  var fn, val;
+  if(S && typeof (fn = it.toString) == 'function' && !isObject(val = fn.call(it)))return val;
+  if(typeof (fn = it.valueOf) == 'function' && !isObject(val = fn.call(it)))return val;
+  if(!S && typeof (fn = it.toString) == 'function' && !isObject(val = fn.call(it)))return val;
+  throw TypeError("Can't convert object to primitive value");
+};
+
+/***/ }),
+/* 19 */
+/* unknown exports provided */
+/* all exports used */
+/*!*****************************************************************!*\
+  !*** ./~/core-js/library/modules/es6.object.define-property.js ***!
+  \*****************************************************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+var $export = __webpack_require__(/*! ./_export */ 14);
+// 19.1.2.4 / 15.2.3.6 Object.defineProperty(O, P, Attributes)
+$export($export.S + $export.F * !__webpack_require__(/*! ./_descriptors */ 0), 'Object', {defineProperty: __webpack_require__(/*! ./_object-dp */ 5).f});
+
+/***/ }),
+/* 20 */,
+/* 21 */
+/* unknown exports provided */
+/* all exports used */
+/*!*********************!*\
+  !*** ./app/home.js ***!
+  \*********************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty__ = __webpack_require__(/*! babel-runtime/helpers/defineProperty */ 6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__welcome__ = __webpack_require__(/*! ./welcome */ 7);
+
+
+
+
+var x = 'x';
+var y = 'y';
+console.log('welcome: ', __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__welcome__["a" /* welcome */])('home'), __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_defineProperty___default()({}, x + '_' + y, '???'));
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=home.bundle.js.map
