a6988a0090792bca79c662b0678fe2126e0825e6
Vasily Krylov <tor@outsorcerers.com>
2017-07-19 02:23:02 +0300

    Cannot read property classes of undefined

--- src/client/components/page-header/index.jsx
+++ src/client/components/page-header/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import injectSheet from 'react-jss'
 
-import styles from './styles'
+const styles = {
+  title: {
+    fontFamily: '\'Roboto Slab\', serif',
+    fontSize: '32px',
+    margin: '20px auto 0 auto',
+    padding: '0 20px',
+    display: 'inline-block',
+    color: 'green',
+  },
+}
 
 const PageHeader = props => (
   <header>
-    <h1 className={styles.title}>
+    <h1 className={props.classes.title}>
       {props.title}
     </h1>
-    
+
     {props.children}
   </header>
 )
 
-export default PageHeader
+export default injectSheet(styles)(PageHeader)
